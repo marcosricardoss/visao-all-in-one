315acfa1a800748c13b6c86b1bc7bc7a2a27621b
Marcos Ricardo <marcosricardoss@gmail.com>
2021-04-13 13:08:11 -0300

    refactor: update the architecture and the app to make sure that just on task will be executing.

--- app/blueprint/api.py
+++ app/blueprint/api.py
@@ -18,21 +18,41 @@ def press_button():
         if task.state == "PENDING" or task.state=="PROGRESS":
             return make_response(jsonify({
                 "msg": "there is another task running",                
+                "task": task.id
             }), 429)  
     
     # creating a new task
     task = long_task.apply_async()     
     return make_response(jsonify({
-        "task_state": str(task.state),
-        "taskID": str(task.id),
-    }), 200)      
+        "state": task.state,
+        "id": task.id,
+    }), 200)    
+
 
 @bp.route('/', methods=['GET'])
 def get_status():         
-    from app.tasks import long_task
-    
     # checking for a running task
     task_id = r.get('taskid')
+    if not task_id:
+        return make_response(jsonify({
+            "msg": "there is no task running",            
+        }), 400)      
+
+    # retreiving the task states
+    from app.tasks import long_task
+    task = long_task.AsyncResult(task_id)
+    return make_response(jsonify({
+        "task_id": task.id,
+        "task_state": task.state,
+        "data": task.info
+    }), 200)
+
+
+@bp.route('/inspect/<string:task_id>', methods=['GET'])
+def inspect(task_id):         
+    from app.tasks import long_task
+    
+    # checking for a running task    
     if not task_id:
         return make_response(jsonify({
             "msg": "there is no task running",
@@ -41,7 +61,6 @@ def get_status():
     # retreiving the task states
     task = long_task.AsyncResult(task_id)
     return make_response(jsonify({
-        "task_state": str(task.state),
-        "data": task.info
-    }), 200)
-    
+        "state": str(task.state),        
+        "info": task.info
+    }), 200)     
