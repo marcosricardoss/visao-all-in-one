3bd13e87b1130074b21aa5a1567dbb0c997fd905
Juliana Midlej <jmesanto91@gmail.com>
2021-05-13 16:55:10 -0300

    Fix imports

--- app/visao/yolov3/yolov4.py
+++ app/visao/yolov3/yolov4.py
@@ -17,6 +17,53 @@ from app.visao.yolov3.configs import *
 STRIDES         = np.array(YOLO_STRIDES)
 ANCHORS         = (np.array(YOLO_ANCHORS).T/STRIDES).T
 
+def filter_boxes(box_xywh, scores, score_threshold=0.4, input_shape=(416, 416), image_shape=(416, 416)):
+    # Deixar só a classe com o maior score
+    scores_max = np.amax(scores, axis=1)
+    # print(scores_max.shape)
+    image_h = input_shape[0]
+    image_w = input_shape[1]
+
+    # Indices das detecções válidas
+    indices = np.argwhere(scores_max >= score_threshold)
+    # print(indices)
+    # print(box_xywh.shape)
+
+    # (x1, y1), (x2, y2), score, class
+    bboxes = []
+    for item in indices:
+        box = []
+        # Coordenadas
+        x = box_xywh[item][0][0] 
+        y = box_xywh[item][0][1]
+        w = box_xywh[item][0][2] 
+        h = box_xywh[item][0][3]
+
+        # print(x)
+        # print(y)
+        # print(w)
+        # print(h)
+
+        xmin = (x-int(w/2))
+        ymin = (y-int(h/2))
+        xmax = (x+int(w/2))
+        ymax = (y+int(h/2))
+
+        box.append(xmin)
+        box.append(ymin)
+        box.append(xmax)
+        box.append(ymax)
+        # Score
+        box.append(scores_max[item][0])
+        # Classe
+        box.append(np.argmax(scores[item]))
+        bboxes.append(box)
+    bboxes = np.asarray(bboxes)
+    # print(bboxes)
+    # print(bboxes.shape)
+
+    return bboxes
+
 def read_class_names(class_file_name):
     # loads class name from a file
     names = {}
