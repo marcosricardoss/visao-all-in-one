bd8c784059cdd2fdf4a18d3b2457c75720c09641
Juliana Midlej <jmesanto91@gmail.com>
2021-05-13 15:32:10 -0300

    TFLITE detection settings

--- app/tasks.py
+++ app/tasks.py
@@ -9,10 +9,6 @@ import cv2 as cv
 import os
 # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 import numpy as np
-import tensorflow as tf
-from app.visao.yolov3.utils import detect_image
-from app.visao.yolov3.configs import *
-from app.visao.yolov3.yolov4 import Create_Yolo
 from app.visao.preprocessing.segment_pcbs import *
 from threading import Thread
 from datetime import datetime
@@ -23,7 +19,7 @@ celery = create_celery_app()
 logger = get_task_logger(__name__)
 r = Redis(host='all-in-one-redis', port=6379, db=0, decode_responses=True)
 
-def makeDetection(frame, yolo, class_models, screw_cascade):
+def makeDetection(frame, yolo1, yolo2, screw_cascade):
     
     # Inicializa array
     components = np.ones((6, 3))
@@ -73,53 +69,6 @@ def makeDetection(frame, yolo, class_models, screw_cascade):
             # Coordenadas do bounding box
             (x1, y1), (x2, y2) = (coor[0], coor[1]), (coor[2], coor[3])
             
-            # Classificar se está correto ou incorreto
-            prediction = np.array([[1.]])
-            if class_ind in range(3):
-                try:
-                   component = image[y1-5:y2+5,x1-5:x2+5,:]
-                   component = cv.resize(component, (32, 32))
-                   component = cv.cvtColor(component, cv.COLOR_BGR2GRAY)
-                   prediction = class_models[str(class_ind)](component[np.newaxis,...,np.newaxis])
-                except:
-                   print("An exception occurred")
-
-            image_y = image.shape[0]
-            if class_ind in range(2):
-                if  (y1+y2)/2 < image_y/3.2:
-                    # AZUL 1
-                    if class_ind == 1 or prediction[0][0] < 0.5:
-                        correct = 3
-                    else:
-                        correct = 0
-                    components[0][placa[index]] = correct
-                    components[0][2] = components_names['0']
-                elif (y1+y2)/2 < image_y/2:
-                    # ROXO 1
-                    if class_ind == 0 or prediction[0][0] < 0.5:
-                        correct = 3
-                    else:
-                        correct = 0
-                    components[1][placa[index]] = correct
-                    components[1][2] = components_names['1']
-                else:
-                    # ROXO 2
-                    if class_ind == 0 or prediction[0][0] < 0.5:
-                        correct = 3
-                    else:
-                        correct = 0
-                    components[2][placa[index]] = correct
-                    components[2][2] = components_names['2']
-            
-            # Componente está incorreto
-            else:
-                if prediction[0][0] < 0.5:
-                    correct = 3
-                else:
-                    correct = 0
-                components[class_ind+1][placa[index]] = correct
-                components[class_ind+1][2] = components_names[str(class_ind+1)]
-
             # Desenhar retângulo e score
             bbox_thick = 1
             fontScale = 0.75 * bbox_thick
@@ -134,16 +83,59 @@ def makeDetection(frame, yolo, class_models, screw_cascade):
             # put text above rectangle
             cv.putText(image, score, (x1, y1-4), cv.FONT_HERSHEY_COMPLEX_SMALL,
                         fontScale, text_colors[str(correct)], 1, lineType=cv.LINE_AA)
-    
-    def detect(index, image):
-        bboxes = detect_image(yolo, image, input_size=YOLO_INPUT_SIZE, CLASSES=TRAIN_CLASSES,
-                                score_threshold=0.5, iou_threshold=0.3)
+
+    def detect1(index, image):
+        image_data = cv.resize(image, (416, 416))
+        image_data = image_data / 255.
+
+        images_data = []
+        for i in range(1):
+            images_data.append(image_data)
+        images_data = np.asarray(images_data).astype(np.float32)
+
+        input_details = yolo1.get_input_details()
+        output_details = yolo1.get_output_details()
+
+        yolo1.set_tensor(input_details[0]['index'], images_data)
+        yolo1.invoke()
+
+        predictions = [yolo1.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
+        bboxes_xyhw = np.asarray(predictions[0])
+        scores = np.asarray(predictions[1])
+        bboxes_xyhw = np.reshape(bboxes_xyhw, (bboxes_xyhw.shape[1], bboxes_xyhw.shape[2]))
+        scores = np.reshape(scores, (scores.shape[1], scores.shape[2]))
+        bboxes = filter_boxes(bboxes_xyhw, scores, score_threshold=0.25, input_shape=(416, 416))
+        
         draw_bboxes(index, image, bboxes)
 
-    thread_1 = Thread(target=detect,args=['left', pcb_left])
+    def detect2(index, image):
+        image_data = cv.resize(image, (416, 416))
+        image_data = image_data / 255.
+
+        images_data = []
+        for i in range(1):
+            images_data.append(image_data)
+        images_data = np.asarray(images_data).astype(np.float32)
+        
+        input_details = yolo2.get_input_details()
+        output_details = yolo2.get_output_details()
+
+        yolo2.set_tensor(input_details[0]['index'], images_data)
+        yolo2.invoke()
+
+        predictions = [yolo2.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
+        bboxes_xyhw = np.asarray(predictions[0])
+        scores = np.asarray(predictions[1])
+        bboxes_xyhw = np.reshape(bboxes_xyhw, (bboxes_xyhw.shape[1], bboxes_xyhw.shape[2]))
+        scores = np.reshape(scores, (scores.shape[1], scores.shape[2]))
+        bboxes = filter_boxes(bboxes_xyhw, scores, score_threshold=0.25, input_shape=(416, 416))
+        
+        draw_bboxes(index, image, bboxes)
+
+    thread_1 = Thread(target=detect1,args=['left', pcb_left])
 
     thread_1.start()
-    detect('right', pcb_right)
+    detect2('right', pcb_right)
 
     thread_1.join()
 
@@ -162,20 +154,13 @@ def long_task(self):
     self.update_state(state='INITIALIZING', meta={"step":step, "components":components})
 
     # Carregar a rede neural YOLO
-    checkpoints_path = "app/visao/checkpoints/yolov3_C920-13all-50epochs_Tiny"
-    yolo = Create_Yolo(input_size=416, CLASSES="app/visao/model_data/classes.txt")
-    yolo.load_weights(checkpoints_path)
+    yolo1 = tflite.Interpreter('./tf-lite/yolov4-tiny-416.tflite')
+    yolo1.allocate_tensors()
+    yolo2 = tflite.Interpreter('./tf-lite/yolov4-tiny-416.tflite')
+    yolo2.allocate_tensors()
     screw_cascade = cv.CascadeClassifier()
     screw_cascade.load(cv.samples.findFile("app/visao/screw_cascade.xml"))
     
-
-    # Carregar modelos de classificação 
-    class_models = {
-        '0': tf.keras.models.load_model('app/visao/classification_models/azul-roxo'),
-        '1': tf.keras.models.load_model('app/visao/classification_models/azul-roxo'),
-        '2': tf.keras.models.load_model('app/visao/classification_models/pequeno')
-    }
-
     # Carregar pasta das imagens
     DEFAULT_MEDIA_FOLDER = os.environ.get("DEFAULT_MEDIA_FOLDER")
 
@@ -208,7 +193,7 @@ def long_task(self):
             date = datetime.now().strftime('%m_%d_%Y - %H:%S')
             cv.imwrite(DEFAULT_MEDIA_FOLDER+"_"+date+"_frame.png", frame)
             components.clear()
-            pcbR,pcbL,components = makeDetection(frame, yolo, class_models, screw_cascade)
+            pcbR,pcbL,components = makeDetection(frame, yolo1, yolo2, screw_cascade)
             if pcbR is pcbL:
                 step = 5
                 self.update_state(state="PCBS WERE NOT FOUND!", meta={"step":step, "components":components})
